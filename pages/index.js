d97621aa60c70ec8d0c2a30b769698bb852b7e2f
Anuj5588 <ak4826636@gmail.com>
2025-11-13 21:50:20 +0530

    made reponsible to category and image carusel component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Category from "../components/category/index"
 import NextJsCarousel from "../components/imageCrauselComponent"
 import Navbar from "../components/Navbar/index"
+import SimpleSlider from "../components/TopDeals"
 import Footer from "../components/Footer/index"
 
 
@@ -9,9 +10,10 @@ import Footer from "../components/Footer/index"
 export default function Home(){
     return(
         <>
-        <div className="h-[800px] bg-gray-200  w-[100%]">
+        <div className="h-[800px] bg-gray-200  w-[100%] m-[0px]">
    <Navbar/>
    <Category/>
+   {/* <SimpleSlider/> */}
    <NextJsCarousel/>
    <Footer/>
    </div>
